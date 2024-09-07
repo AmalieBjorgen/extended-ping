b382f20c0960182086b7590c85ecf44a996518ed
Amalie Bjørgen <amalie.bjorgen@bkk.no>
2024-09-07 17:45:29 +0200

    attempting to make icmp ping

--- extended-ping.go
+++ extended-ping.go
@@ -4,21 +4,27 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"os"
 	"time"
+
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
 )
 
 func main() {
-	timeout := 15
+	var timeout time.Duration = time.Second * 3
 	host := "google.com"
 	port := "80"
 
-	tcp_ping(host, port, time.Duration(timeout)*time.Second)
-	udp_ping(host, port, time.Duration(timeout)*time.Second)
+	tcp_ping(host, port, timeout)
+	icmp_ping(host)
+	//udp_ping(host, port, timeout)
 
 }
 
 func tcp_ping(host string, port string, timeout time.Duration) {
-	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%v:%v", host, port), timeout)
+	d := net.Dialer{Timeout: timeout}
+	conn, err := d.Dial("tcp", fmt.Sprintf("%v:%v", host, port))
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
@@ -34,7 +40,8 @@ func tcp_ping(host string, port string, timeout time.Duration) {
 }
 
 func udp_ping(host string, port string, timeout time.Duration) {
-	conn, err := net.DialTimeout("udp", fmt.Sprintf("%v:%v", host, port), timeout)
+	d := net.Dialer{Timeout: timeout}
+	conn, err := d.Dial("udp", fmt.Sprintf("%v:%v", host, port))
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
@@ -47,4 +54,72 @@ func udp_ping(host string, port string, timeout time.Duration) {
 		return
 	}
 	fmt.Println("UDP ping successful\n", status)
+}
+
+func icmp_ping(host string) {
+	ip, err := net.ResolveIPAddr("ip4", host)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	conn, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	defer conn.Close()
+
+	msg := icmp.Message{
+		Type: ipv4.ICMPTypeEcho,
+		Code: 0,
+		Body: &icmp.Echo{
+			ID:   os.Getpid() & 0xffff,
+			Seq:  1,
+			Data: []byte(""),
+		},
+	}
+	msg_bytes, err := msg.Marshal(nil)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	if _, err := conn.WriteTo(msg_bytes, &net.UDPAddr{IP: ip.IP}); err != nil {
+		fmt.Println("Error: ", err)
+		panic(err)
+	}
+
+	err = conn.SetReadDeadline(time.Now().Add(time.Second * 1))
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	reply := make([]byte, 1500)
+	n, _, err := conn.ReadFrom(reply)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	parsed_reply, err := icmp.ParseMessage(1, reply[:n])
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	switch parsed_reply.Code {
+	case 0:
+		// Got a reply so we can save this
+		fmt.Printf("Got Reply from %s\n", host)
+	case 3:
+		fmt.Printf("Host %s is unreachable\n", host)
+		// Given that we don't expect google to be unreachable, we can assume that our network is down
+	case 11:
+		// Time Exceeded so we can assume our network is slow
+		fmt.Printf("Host %s is slow\n", host)
+	default:
+		// We don't know what this is so we can assume it's unreachable
+		fmt.Printf("Host %s is unreachable\n", host)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module extended-ping
 
 go 1.23.0
+
+require golang.org/x/net v0.29.0
+
+require golang.org/x/sys v0.25.0 // indirect
